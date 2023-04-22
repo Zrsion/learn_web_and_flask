e686b4745137766098a44e8777857a15a0759bf6
Zrsion <1079451538@qq.com>
2023-04-22 15:17:53 +0800

    update

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+list = [
+    {
+    'id':1,
+    'title':"first",
+    'text':"from the first one"
+    },
+    {
+    'id':2,
+    'title':"second",
+    'text':"from the second one"
+    }
+]
+
+
 @app.route("/")
 def Try():
-    return render_template('home.html')
+    return render_template('home.html',list = list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
